324c41056ac62979fdfee1af314cfbf46f15122c
aarri002 <aarri002@ucr.edu>
2015-06-06 08:08:56 -0700

    header
    Project Files

---
+++ include/notes.h
@@ -0,0 +1,55 @@
+#ifndef __NOTES_H__
+#define __NOTES_H__
+
+
+#define LOW 1
+#define MID 2
+#define HIGH 3
+
+//Music Constants
+#define OO 0
+
+#define C2 65.406
+#define C2S 69.296
+#define D2 73.416
+#define D2S 77.782
+#define E2 82.407
+#define F2 87.307
+#define F2S 92.499
+#define G2 97.999
+#define G2S 103.83
+#define A2 110.000
+#define A2S 116.54
+#define B2 123.470
+
+#define C3 130.810
+#define C3S 138.590
+#define D3 146.830
+#define D3S 155.560
+#define E3 164.810
+#define F3 174.610
+#define F3S 185.000
+#define G3 196.000
+#define G3S 207.650
+#define A3 220.000
+#define A3S 233.080
+#define B3 246.940
+
+#define C4 261.630
+#define C4S 277.180
+#define D4 263.660
+#define D4S 311.130
+#define E4 329.630
+#define F4 349.230
+#define F4S 369.990
+#define G4 392.000
+#define G4S 415.300
+#define A4 440.000
+#define A4S 466.160
+#define B4 493.880
+
+#define C5 523.250
+
+#define MUSIC_END 1.000
+
+#endif 
